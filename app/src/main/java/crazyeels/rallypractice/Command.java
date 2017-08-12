d310fc9270a47113d8069e8b65476a750871db66
Grace Stuart <gracerstuart@gmail.com>
2017-08-12 14:18:07 -0700

    declare and initialize variables, add onclickListener to button

--- app/src/main/java/crazyeels/rallypractice/Command.java
+++ app/src/main/java/crazyeels/rallypractice/Command.java
@@ -2,12 +2,31 @@ package crazyeels.rallypractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Command extends AppCompatActivity {
+
+    private TextView commandTextView;
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_command);
+
+        commandTextView = (TextView) findViewById(R.id.content);
+        nextButton = (Button) findViewById(R.id.mailButton);
+
+        View.OnClickListener nextCommand = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        } ;
+
+        nextButton.setOnClickListener(nextCommand);
     }
 }
