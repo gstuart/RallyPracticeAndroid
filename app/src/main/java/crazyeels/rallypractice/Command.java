96f73c45659c15c8581169b1f52bc08ea3e8b749
Grace Stuart <gracerstuart@gmail.com>
2017-08-12 13:15:57 -0700

    Initial commit

---
+++ app/src/main/java/crazyeels/rallypractice/Command.java
@@ -0,0 +1,13 @@
+package crazyeels.rallypractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Command extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_command);
+    }
+}
