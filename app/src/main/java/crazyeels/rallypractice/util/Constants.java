1a575fa61b49cb07debaf442ad65e65ed5e96ef7
Grace Stuart <gracerstuart@gmail.com>
2017-08-12 21:49:03 -0700

    add strings for urls

--- app/src/main/java/crazyeels/rallypractice/util/Constants.java
+++ app/src/main/java/crazyeels/rallypractice/util/Constants.java
@@ -2,7 +2,7 @@ package crazyeels.rallypractice.util;
 
 public final class Constants {
   /* Nav Drawer */
-    public final static String youtube_url =  "x";
-    public final static String google_play_store_prefix = "";
+    public final static String youtube_url = "https://www.youtube.com/watch?v=3wrPZcKXR0E&list=PL7jyQ4vQtEeKAWa8DMRRJj3gPOmJmFmhG";
+    public final static String akc_url = "http://images.akc.org/pdf/rulebooks/ROR001.pdf";
 
 }
