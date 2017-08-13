15962d08f55ec581ec1e7de60d6ed5ab3d2183cd
Grace Stuart <gracerstuart@gmail.com>
2017-08-12 19:23:41 -0700

    make default orientation as portrait

--- app/src/main/java/crazyeels/rallypractice/util/Splash.java
+++ app/src/main/java/crazyeels/rallypractice/util/Splash.java
@@ -1,9 +1,10 @@
-package crazyeels.rallypractice;
+package crazyeels.rallypractice.util;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import crazyeels.rallypractice.R;
 import crazyeels.rallypractice.ui.Command;
 
 public class Splash extends AppCompatActivity {
