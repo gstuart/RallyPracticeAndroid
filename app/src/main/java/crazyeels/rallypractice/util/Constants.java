ffdcb27f9e3aa9cc24b55e6340bdb454f973e0be
Grace Stuart <gracerstuart@gmail.com>
2017-08-12 21:14:10 -0700

    create constants file

---
+++ app/src/main/java/crazyeels/rallypractice/util/Constants.java
@@ -0,0 +1,8 @@
+package crazyeels.rallypractice.util;
+
+public final class Constants {
+  /* Nav Drawer */
+    public final static String youtube_url =  "x";
+    public final static String google_play_store_prefix = "";
+
+}
