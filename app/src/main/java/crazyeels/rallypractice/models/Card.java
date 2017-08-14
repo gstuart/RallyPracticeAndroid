15379e8a1008176d1ce1a0d72264c4b608513482
Grace Stuart <gracerstuart@gmail.com>
2017-08-14 16:37:51 -0700

    create card model

---
+++ app/src/main/java/crazyeels/rallypractice/models/Card.java
@@ -0,0 +1,33 @@
+package crazyeels.rallypractice.models;
+
+import java.net.URL;
+
+public class Card {
+    private Integer cardNumber;
+    private String cardDetail;
+    private String level;
+    private URL image;
+
+    public Card(Integer number, String detail, String level, URL image) {
+        this.cardNumber = number;
+        this.cardDetail = detail;
+        this.level = level;
+        this.image = image;
+    }
+
+    public Integer getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getCardDetail() {
+        return cardDetail;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public URL getImage() {
+        return image;
+    }
+}
