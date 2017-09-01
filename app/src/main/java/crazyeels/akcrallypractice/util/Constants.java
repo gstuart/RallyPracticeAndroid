02dd69af7f92d9d8e88198b7b34046babc760dcd
Grace Stuart <gracerstuart@gmail.com>
2017-09-01 07:10:59 -0700

    change youtube url to akc site

--- app/src/main/java/crazyeels/akcrallypractice/util/Constants.java
+++ app/src/main/java/crazyeels/akcrallypractice/util/Constants.java
@@ -2,7 +2,7 @@ package crazyeels.akcrallypractice.util;
 
 public final class Constants {
   /* Nav Drawer */
-    public final static String youtube_url = "https://www.youtube.com/watch?v=3wrPZcKXR0E&list=PL7jyQ4vQtEeKAWa8DMRRJj3gPOmJmFmhG";
+    public final static String youtube_url = "https://www.youtube.com/watch?v=gXH5VegJ4ZA&list=PLpkaTxdGkvI3lFUBckw-NS4BXxVBzwK1_";
     public final static String akc_url = "http://images.akc.org/pdf/rulebooks/ROR001.pdf";
     public final static String support_email = "crazyeelsapps@gmail.com";
     public final static String google_play_url = "https://play.google.com/store/apps/details?id=";
