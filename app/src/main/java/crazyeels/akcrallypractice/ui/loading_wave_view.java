3364542969e98ceb7dcf3d8a0934c9fecedcc319
Grace Stuart <gracerstuart@gmail.com>
2017-09-02 18:34:56 -0700

    add layout for wave loading

---
+++ app/src/main/java/crazyeels/akcrallypractice/ui/loading_wave_view.java
@@ -0,0 +1,15 @@
+package crazyeels.akcrallypractice.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import crazyeels.akcrallypractice.R;
+
+public class loading_wave_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_wave_view);
+    }
+}
