563cdba56d0ec993dfe9d3ca978e00477dae4ea3
Grace Stuart <gracerstuart@gmail.com>
2017-09-02 18:51:42 -0700

    add functionality

--- app/src/main/java/crazyeels/akcrallypractice/ui/loading_wave_view.java
+++ app/src/main/java/crazyeels/akcrallypractice/ui/loading_wave_view.java
@@ -2,14 +2,56 @@ package crazyeels.akcrallypractice.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 import crazyeels.akcrallypractice.R;
+import me.itangqi.waveloadingview.WaveLoadingView;
 
 public class loading_wave_view extends AppCompatActivity {
+
+    WaveLoadingView waveLoadingView;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_wave_view);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        waveLoadingView = (WaveLoadingView) findViewById(R.id.waveLoadingView);
+        waveLoadingView.setProgressValue(0);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                waveLoadingView.setProgressValue(progress);
+                if(progress < 50) {
+                    waveLoadingView.setBottomTitle(String.format("%d%%", progress));
+                    waveLoadingView.setCenterTitle("");
+                    waveLoadingView.setTopTitle("");
+                }
+                else if(progress < 80) {
+                    waveLoadingView.setBottomTitle("");
+                    waveLoadingView.setCenterTitle(String.format("%d%%", progress));
+                    waveLoadingView.setTopTitle("");
+                }
+                else {
+                    waveLoadingView.setBottomTitle("");
+                    waveLoadingView.setCenterTitle("");
+                    waveLoadingView.setTopTitle(String.format("%d%%", progress));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
